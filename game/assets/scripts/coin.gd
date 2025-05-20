6f84f790bea4dfe61be83491fc93f2092b478e9b
jusMarz <jonas.aj.08@gmail.com>
2025-05-20 10:31:13 -0400

    -Updated Coin Script

--- game/assets/scripts/coin.gd
+++ game/assets/scripts/coin.gd
@@ -1,25 +1,31 @@
 extends Area2D
+
 @onready var player = %Player
 @onready var audio_stream_player_2d = $AudioStreamPlayer2D
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var collision_shape_2d = $CollisionShape2D
 
-enum State {Idle,Aggresive}
-@export var state : State
-var target_node
-var SPEED = 10;
+var chase = false
+var speed = 3.5
 
+func get_angle_to_target_node():
+	return (abs(atan2((-position.y + player.position.y),(-position.x + player.position.x))))
+	
 func _process(delta):
-	var distance = sqrt(pow((player.position.y - position.y),2) + pow((player.position.x - position.x),2))
+	
+	var distance = sqrt(pow((player.position.y - position.y ),2) + pow((player.position.x - position.x),2))
 	if distance < 50:
-		state = State.Aggresive
+		chase = true
+	if distance < 5:
+			queue_free()
+			
+	if chase:
+		var angle = get_angle_to_target_node()
+		position.x += (int(position.x < player.position.x) - int(position.x > player.position.x)) * delta * 30 * abs(cos(angle)) * speed
+		position.y += (int(position.y < player.position.y) - int(position.y > player.position.y)) * delta * 30 * abs(sin(angle)) * speed
+		speed += .2 * delta
+		audio_stream_player_2d.play()
 		
-	match state:
-		State.Aggresive:
-			var angle = (abs(atan2((-position.y + player.position.y),(-position.x + player.position.x))))
-			position.x += ((-position.x + player.position.x)/abs(position.x - player.position.x)) * delta * 30 * abs(cos(angle)) * SPEED
-			position.y += ((-position.y + player.position.y)/abs(position.y - player.position.y)) * delta * 30 * abs(sin(angle)) * SPEED
-			audio_stream_player_2d.play()
 	
 
-func _on_body_entered(body):
-	print("got coin")
-	queue_free()
+
