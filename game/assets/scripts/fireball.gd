0944605bafd5df53991827a057fe375a84eb04b6
AdnerC <148999774+AdnerC@users.noreply.github.com>
2025-05-30 10:51:03 -0400

    Add files via upload

--- game/assets/scripts/fireball.gd
+++ game/assets/scripts/fireball.gd
@@ -11,12 +11,14 @@ var SPEED = 10
 var angle =0
 var shooting = false
 signal shoots
-signal spawnBall
+
 # Called when the node enters the scene tree for the first time.\
 
 
 func _ready():
 	$"..".connect("shoots", _on_shoots)
+	print("started fireball")
+
 	
 	
 	
@@ -25,31 +27,25 @@ func get_angle_to_target_node():
 	return (atan2(( -player.position.y +  get_global_mouse_position().y),(-player.position.x + get_global_mouse_position().x)))
 
 
-func spawn_object(position):
-	print("object spawned")
-	var new_fireball = object_scene.instantiate()
-	new_fireball.position = position
-	get_tree().root.add_child(new_fireball)
 
 func _on_shoots():
-	timer.start(3)
-	print("timer start")
+	print("shoot signal recieved")
 	shooting = true
 	mousePos = get_global_mouse_position()
 	angle = get_angle_to_target_node()
-	visible = true
-	collision.disabled = false
+	#visible = true
+	#collision.disabled = false
 	print(angle)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if shooting:
+		visible=true
+		animated_sprite.play("fireball")
 		position.x += delta * 30 * cos(angle) * SPEED
 		position.y += delta * 30 * sin(angle) * SPEED
 
 
-func _on_timer_timeout():
-	emit_signal("spawnBall")
 
 
 func _on_fireball_tree_entered():
