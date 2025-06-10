75bf78b87224dd6d5aa3fe149aa1cc5a966b8f34
jusMarz <jonas.aj.08@gmail.com>
2025-06-09 22:07:39 -0400

    Add files via upload

--- game/assets/scripts/Hurtbox.gd
+++ game/assets/scripts/Hurtbox.gd
@@ -3,14 +3,19 @@ extends Area2D
 @onready var parent = get_parent()
 @export var ALIGNMENT: String
 
+
 func _on_area_entered(area):
-	var node = area.get_parent()
-	if ((node.has_method("get_stats"))):
-	
-		print(str(parent) + ": " + str(area)+" up in my area")
-		parent.health = parent.health - node.get_stats(0)
-		parent.stun_meter = parent.stun_meter + node.get_stats(1)
-		parent.knockback = parent.stun_meter + node.get_stats(2)
+	var attack = area.get_parent()
+	if ((attack.has_method("give_stats"))):
+		var stats = attack.give_stats()
+		print(stats)
+		if ALIGNMENT == stats[3]:
+			print("WORKS")
+		else:
+			print(str(parent) + ": " + str(attack)+" up in my area")
+			parent.health = parent.health - stats[0]
+			parent.stun_meter = parent.stun_meter + stats[1]
+			parent.knockback = parent.stun_meter + stats[2]
 	pass # Replace with function body.
 
 
