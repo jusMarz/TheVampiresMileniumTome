c604f9b5fad40f00eae5ec2d9411c9368d8f58b3
jack22325 <jacka49@nycstudents.net>
2025-06-13 10:58:02 -0400

    Add files via upload

--- game/assets/scripts/Hurtbox.gd
+++ game/assets/scripts/Hurtbox.gd
@@ -5,15 +5,15 @@ extends Area2D
 
 
 func _on_area_entered(area):
-	print (str(parent)+"this is the parent " + str(area))
+	#print (str(parent)+"this is the parent " + str(area))
 	var attack = area.get_parent()
 	if ((attack.has_method("give_stats"))):
 		var stats = attack.give_stats()
-		print(stats)
+		#print(stats)
 		if ALIGNMENT == stats[3]:
 			print("WORKS")
 		else:
-			print(str(parent) + ": " + str(attack)+" up in my area")
+			#print(str(parent) + ": " + str(attack)+" up in my area")
 			parent.health = parent.health - stats[0]
 			parent.stun_meter = parent.stun_meter + stats[1]
 			parent.knockback = parent.stun_meter + stats[2]
